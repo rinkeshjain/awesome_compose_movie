5d8199bc7985b6bef9292b9f5e3432d23f0b0505
rinkesh <rinkesh.jain@davintafinserv.com>
2023-03-19 22:45:26 +0530

    added UTILS File

---
+++ app/src/main/java/com/rinkesh/composemovie/utlis/Utils.kt
@@ -0,0 +1,11 @@
+package com.rinkesh.composemovie.utlis
+
+
+object Utils {
+//    TODO UPDATE API KEY
+    const val apiKey = ""
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w780/"
+    const val BASE_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+}
+
